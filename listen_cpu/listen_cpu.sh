c4b4deea4f5f946eae053320f6a8dde93264b5ad
a5030700as <756246608@qq.com>
2020-11-04 06:22:37 -0800

    cpu监控脚本

---
+++ listen_cpu/listen_cpu.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+for each in {1..10}
+do
+    echo "hello $each"
+done
+
+wodiu=1
+counter=0 
+for ((;;))
+do 
+    echo  hello cpu
+    times=`top -b -n 1 | grep "^%[Cc]pu" | awk '$2+$4 >= 0.3 {print "1"; }'`
+    echo $times
+    
+    #top -b -n 1 | grep "^%[Cc]pu" | awk '$2+$4 >= 0.3 {print "cpu超出了"; system("wodiu=5")}'
+    if [ $times -ge 1 ]
+    then 
+        let counter++
+        echo "let $counter"   
+    else
+        counter=0;    
+    fi
+    if [ $counter -ge 10 ]
+    then 
+        echo "ready for reboot"
+    fi    
+    sleep 1
+done
